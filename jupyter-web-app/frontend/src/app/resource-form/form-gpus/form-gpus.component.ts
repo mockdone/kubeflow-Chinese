f249c2fb0285a0f2e72161d81e1fcde78bbb232f
mockdone <690033956@qq.com>
2020-06-29 13:34:12 +0800

    初始化提交

---
+++ jupyter-web-app/frontend/src/app/resource-form/form-gpus/form-gpus.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {FormGroup, ValidatorFn, AbstractControl} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {GPUVendor} from 'src/app/utils/types';
+
+@Component({
+  selector: 'app-form-gpus',
+  templateUrl: './form-gpus.component.html',
+  styleUrls: ['./form-gpus.component.scss', '../resource-form.component.scss'],
+})
+export class FormGpusComponent implements OnInit {
+  @Input() parentForm: FormGroup;
+  @Input() vendors: GPUVendor[];
+  private gpuCtrl: FormGroup;
+  subscriptions = new Subscription();
+
+  maxGPUs = 16;
+  gpusCount = ['256m', '512m', '1024m', '2048m', '4096m'];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.gpuCtrl = this.parentForm.get('gpus') as FormGroup;
+
+    // Vendor should not be empty if the user selects GPUs num
+    this.parentForm
+      .get('gpus')
+      .get('vendor')
+      .setValidators([this.vendorWithNum()]);
+
+    this.subscriptions.add(
+      this.gpuCtrl.get('num').valueChanges.subscribe((n: string) => {
+        if (n === 'none') {
+          this.gpuCtrl.get('vendor').disable();
+        } else {
+          this.gpuCtrl.get('vendor').enable();
+        }
+      }),
+    );
+  }
+
+  // Custom Validation
+  public getVendorError() {
+    const vendorCtrl = this.parentForm.get('gpus').get('vendor');
+
+    if (vendorCtrl.hasError('vendorNullName')) {
+      return `请选择GPU厂商`;
+    }
+  }
+
+  private vendorWithNum(): ValidatorFn {
+    // Make sure that if the user has specified a number of GPUs
+    // that they also specify the GPU vendor
+    return (control: AbstractControl): { [key: string]: any } => {
+      const num = this.parentForm.get('gpus').get('num').value;
+      const vendor = this.parentForm.get('gpus').get('vendor').value;
+
+      if (num !== 'none' && vendor === '') {
+        return {vendorNullName: true};
+      } else {
+        return null;
+      }
+    };
+  }
+}
